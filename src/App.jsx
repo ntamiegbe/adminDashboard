5658e3bead94b354826a0e002aa8c129727454e3
Ntami Egbe <ntamiegbe00@gmail.com>
2023-01-13 21:12:38 +0100

    Commit: Setting up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import New from './pages/new/New'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Single from './pages/single/Single'
+import List from './pages/list/List'
 
 function App() {
 
   return (
     <div className="App">
-      Start
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='users'>
+              <Route index element={<List />} />
+              <Route path=':userId' element={<Single />} />
+              <Route path='new' element={<New />} />
+            </Route>
+            <Route path='products'>
+              <Route index element={<List />} />
+              <Route path=':productId' element={<Single />} />
+              <Route path='new' element={<New />} />
+            </Route>
+          </Route>
+      </Routes>
+    </BrowserRouter>
+    </div >
   )
 }
 
