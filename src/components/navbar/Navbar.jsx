c59df22d69e46034389d8fd82efb1beb47d5c7b8
Ntami Egbe <ntamiegbe00@gmail.com>
2023-01-14 08:52:43 +0100

    C0mmit: Creating Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,48 @@
+import SearchIcon from '@mui/icons-material/Search';
 import "./navbar.scss"
 
 import React from 'react'
+import { ChatBubbleOutlineOutlined, DarkModeOutlined, FullscreenExitOutlined, LanguageOutlined, ListOutlined, NotificationsNoneOutlined } from '@mui/icons-material';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <div className="wrapper">
+        <div className="text">
+          <h4>Good Morning, <span>John Doe</span></h4>
+        </div>
+        <div className="icons">
+          <div className="search">
+            <input type="text" placeholder="Search..." />
+            <SearchIcon />
+          </div>
+          <div className="icon">
+            <LanguageOutlined />
+            English
+          </div>
+          <div className="icon">
+            <DarkModeOutlined />
+          </div>
+          <div className="icon">
+            <FullscreenExitOutlined />
+          </div>
+          <div className="icon">
+            <NotificationsNoneOutlined />
+            <div className="counter">6</div>
+          </div>
+          <div className="icon">
+            <ChatBubbleOutlineOutlined />
+            <div className="counter">2</div>
+          </div>
+          <div className="icon">
+            <ListOutlined />
+          </div>
+          <div className="icon">
+            <img src="https://images.pexels.com/photos/6626903/pexels-photo-6626903.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" className='avatar'/>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
