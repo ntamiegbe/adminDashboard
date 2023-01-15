27831f5bf9d197e597f1fdab459eaf43a60262a7
Ntami Egbe <ntamiegbe00@gmail.com>
2023-01-15 15:50:58 +0100

    Commit: Created datatable component

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./list.scss"
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Datatable from '../../components/dataTable/DataTable'
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className='list'>
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
   )
 }
 
